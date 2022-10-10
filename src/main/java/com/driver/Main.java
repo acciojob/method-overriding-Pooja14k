26ac7ed454a4f73acef40ba0cb61765873d4ab19
Pooja Kale <99339372+Pooja14k@users.noreply.github.com>
2022-10-10 14:59:53 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,12 @@ public class Main {
         B obj = new B();
         System.out.println(obj.meth());
     }
-  static class A {
+  public static class A {
       public String meth(){
           return "Invoking method from class A";
       }
   }
-  static class B extends A{
+  public static class B extends A{
       public String meth(){
           return "Method is overridden in Extendend class B";
       }
